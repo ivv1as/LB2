bcc7b8cbea0e5957b3d785bac8b01aa74fd01109
ivv1as <91069855+ivv1as@users.noreply.github.com>
2021-11-22 21:34:11 +0300

    Update lb2task2.java

--- lb2task2.java
+++ lb2task2.java
@@ -7,7 +7,7 @@ public class lb2task2 {
         int flag = 0;
         int result = 1;
         do {
-            System.out.println("Введите целое числовое значение, не превышающее 15");
+            System.out.println("Введите целое числовое значение для вычисления факториала, не превышающее 15");
             while (!s.hasNextInt()) {
                 System.out.println("Введите корректные данные");
                 s.next();
@@ -17,7 +17,7 @@ public class lb2task2 {
         }
         while (flag == 0);
 
-        if (a <= 15) {
+        if ((a <= 15)&(a>=0)) {
             for (int i = 1; i <= a; i++) {
                 result = result * i;}
                 System.out.println(result);
